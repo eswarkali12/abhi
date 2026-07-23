ed4fb48c12791f5aecb1e0ba640a75b160f1cf2b
eswarkali12 <abhieswar1713@gmail.com>
2026-07-23 07:20:52 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,10 +2,4 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World! this is jenkins Eswar");
     }
-    jhdvjz
-    zhvjzkh
-    jshgjas
-    jshjsavc
-    shcjzm
-}
 }
