47837637f5b0f9affefe4d47284ec4addbcf0b84
eswarkali12 <abhieswar1713@gmail.com>
2026-07-23 06:56:14 +0530

    Fix formatting and cleanup in HelloWorld.java
    Removed extra characters and fixed formatting issues.

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,10 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World! this is jenkins Eswar");
     }
+    jhdvjz
+    zhvjzkh
+    jshgjas
+    jshjsavc
+    shcjzm
 }
 }
